a040f1810cec59fe58f9c49ef9bfa93ad45b78f4
tashfeendev <tashfeendev@gmail.com>
2023-08-10 01:58:56 +0500

    added small css for task form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { useTaskContext } from "./TaskContext";
+import TaskForm from "./TaskForm";
 
 function App() {
+  const { tasks, addTask, completeTask, deleteTask } = useTaskContext();
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Task Management for funfox</h1>
       </header>
+      <main>
+        <TaskForm addTask={addTask} />
+      </main>
     </div>
   );
 }
