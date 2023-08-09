08d2e3272e36bb3d745ac8abb0d4b4b5d9a06dc1
tashfeendev <tashfeendev@gmail.com>
2023-08-10 01:55:10 +0500

    added task form

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserGroupProvider } from "./UserGroupContext";
+import { TaskProvider } from "./TaskContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserGroupProvider>
-      <App />
+      <TaskProvider>
+        <App />
+      </TaskProvider>
     </UserGroupProvider>
   </React.StrictMode>
 );
