a781fbfad202565bde08ebaddd27d2d114d54fc4
tashfeendev <tashfeendev@gmail.com>
2023-08-11 01:16:12 +0500

    added drab drop function

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,24 +1,40 @@
 import Task from "./Task";
 import { useTaskContext } from "./TaskContext";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const TaskList = ({ groupId }) => {
   const { tasks, completeTask, deleteTask } = useTaskContext();
 
   return (
-    <ul>
-      {tasks &&
-        tasks
-          .filter((task) => task.groupId === groupId)
-          .map((task) => (
-            <Task
-              key={task.id}
-              task={task}
-              completeTask={completeTask}
-              deleteTask={deleteTask}
-              groupId={groupId}
-            />
-          ))}
-    </ul>
+    <DragDropContext>
+      <Droppable droppableId='tasks'>
+        {(provided) => (
+          <ul {...provided.droppableProps} ref={provided.innerRef}>
+            {tasks &&
+              tasks
+                .filter((task) => task.groupId === groupId)
+                .map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                    {(provided) => (
+                      <div
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}>
+                        <Task
+                          task={task}
+                          completeTask={completeTask}
+                          deleteTask={deleteTask}
+                          groupId={groupId}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
